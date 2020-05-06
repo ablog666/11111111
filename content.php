e627fd7cb35c5a60a7faa322603e8b91f48ce23c
ablog666 <54060082+ablog666@users.noreply.github.com>
2020-05-06 21:10:18 +0300

    php

---
+++ content.php
@@ -0,0 +1,61 @@
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row2 bgded" style="background-image:url('images/demo/backgrounds/01.png');">
+  <div class="overlay">
+    <section id="pageintro" class="clear"> 
+      <!-- ################################################################################################ -->
+      <div class="center btmspace-80">
+        <h2 class="heading xxl uppercase btmspace-30">Ближайшие числа</h2>
+
+      </div>
+      <ul class="nospace group services">
+        <li class="one_third first">
+          <article class="overlay"><a href="#"><i class="fa fa-legal"></i></a>
+            <h6 class="heading">05.06</h6>
+            <footer><a href="/11111111/pages/may/5.html">Read More &raquo;</a></footer>
+          </article>
+        </li>
+        <li class="one_third">
+          <article class="overlay"><a href="#"><i class="fa fa-line-chart"></i></a>
+            <h6 class="heading">06.06</h6>
+            <footer><a href="/11111111/pages/may/6.html">Read More &raquo;</a></footer>
+          </article>
+        </li>
+        <li class="one_third">
+          <article class="overlay"><a href="#"><i class="fa fa-archive"></i></a>
+            <h6 class="heading">07.06</h6>
+            <footer><a href="/11111111/pages/may/7.html">Read More &raquo;</a></footer>
+          </article>
+        </li>
+      </ul>
+      <!-- ################################################################################################ -->
+    </section>
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row6">
+  <div id="copyright" class="clear"> 
+    <!-- ################################################################################################ -->
+    <p class="fl_left">Copyright &copy; 2020 - All Rights Reserved - <a href="#">Domain Name</a></p>
+    <p class="fl_right">Template by <a target="_blank" href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a> 
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+</html>
